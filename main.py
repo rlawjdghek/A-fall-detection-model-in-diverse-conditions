be53e81aa4c4546a70e60ba24ebf3db136c0fb64
rlawjdghek <rlawjdghek@skku.edu>
2023-01-20 14:59:29 +0900

    debug

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ sys.path.append("./mission1/human_detect")
 sys.path.append("./mission1/person_classification")
 sys.path.append("./OCR")
 sys.path.append("./mission3")
-## ver1asdasd123123asdasdasdaeesdasdasddfgdfgdfgxcvxcvxvccxvzzzzdddddqweqwennmnbbbbtry54654645645
+## ver1asdasd123123asdasdasdaeesdasdasddfgdfgdfgxcvxcvxvccxvzzzzdddddqweqwennmnbbbbtry54654645645asdasdas
 import os
 from os.path import join as opj
 from glob import glob
