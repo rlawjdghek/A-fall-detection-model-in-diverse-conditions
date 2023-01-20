a130a1e04a1418117428407e3ace0bac177e89ae
rlawjdghek <rlawjdghek@skku.edu>
2023-01-20 14:56:21 +0900

    debug

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ sys.path.append("./mission1/human_detect")
 sys.path.append("./mission1/person_classification")
 sys.path.append("./OCR")
 sys.path.append("./mission3")
-# jho2
+# jho
 import os
 from os.path import join as opj
 from glob import glob
